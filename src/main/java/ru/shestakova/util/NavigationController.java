2eb74ebbc71f5447227f63f8f251a861b5ba8486
Shestakova Sofya <soneshya@mail.ru>
2019-08-14 16:21:51 +0500

    some changes in front

--- src/main/java/ru/shestakova/util/NavigationController.java
+++ src/main/java/ru/shestakova/util/NavigationController.java
@@ -12,41 +12,15 @@ import lombok.Data;
 @ManagedBean(name = "navigationController", eager = true)
 @RequestScoped
 @Data
-public class NavigationController  extends NavigationHandler {
+public class NavigationController extends NavigationHandler {
 
-    public String getPageId() {
-        return pageId;
+
+    public String showIndex() {
+        return "index";
     }
 
-    public void setPageId(String pageId) {
-        this.pageId = pageId;
-    }
-
-    // todo refactor
-    //this managed property will read value from request parameter pageId
-    @ManagedProperty(value = "#{param.pageId}")
-    private String pageId = "2";
-
-    //condional navigation based on pageId
-    //if pageId is 1 show page1.xhtml,
-    //if pageId is 2 show page2.xhtml
-    //else show home.xhtml
-
-    public String showPage() {
-        if (pageId == null) {
-            System.out.println("null");
-            return "home";
-        }
-        if (pageId.equals("1")) {
-            System.out.println("1.home");
-            return "home";
-        } else if (pageId.equals("2")) {
-            System.out.println("2.index");
-            return "index";
-        } else {
-            System.out.println("else.home");
-            return "home";
-        }
+    public String showPlot() {
+        return "plot";
     }
 
     public void handleNavigation(FacesContext facesContext, String s, String s1) {
